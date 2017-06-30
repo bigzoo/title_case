810839841c81e7a05fb019fc5e5cad1bbb3d03ff
Chris Nyaga <nyagachris75@gmail.com>
2017-06-30 07:54:03 +0300

    Application complete, automated specs added and pry has been used for debugging

--- app.rb
+++ app.rb
@@ -1,1 +1,17 @@
-require('pry')
+require('sinatra')
+  require('sinatra/reloader')
+  also_reload('lib/**/*.rb')
+  require('./lib/title_case')
+  require('pry')
+
+  get('/') do
+    erb(:index)
+  end
+  get('/form') do
+    erb(:form)
+  end
+
+  get('/title') do
+    @title = params.fetch('title').title_case()
+    erb(:title)
+  end
